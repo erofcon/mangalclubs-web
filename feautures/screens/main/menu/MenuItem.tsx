a20572e1d99420cc548135e9bec38bc41774f100
erofcon <temirlan.shereuzhev@gmail.com>
2026-04-22 00:30:29 +0300

    added a mobile version of the menu

--- feautures/screens/main/menu/MenuItem.tsx
+++ feautures/screens/main/menu/MenuItem.tsx
@@ -9,16 +9,16 @@ interface MenuItemProps {
 
 export function MenuItem({item}: MenuItemProps) {
     return (
-        <div className="p-10 cursor-pointer shadow-sm hover:shadow-md h-full flex flex-col">
+        <div className="md:p-10 px-4 cursor-pointer shadow-sm hover:shadow-md h-full flex flex-col">
 
-            <div className="relative h-50 w-full flex items-end justify-center overflow-hidden">
+            <div className="relative h-40 md:h-50 w-full flex items-end justify-center overflow-hidden">
 
                 {item.image ? (
                     <Image
                         src={item.image}
                         alt={item.name}
                         fill
-                        className="object-contain object-bottom p-2 transition-all duration-500 hover:translate-y-2"
+                        className="object-contain object-bottom md:p-2 transition-all duration-500 hover:translate-y-2"
                     />
                 ) : (
                     <span className="text-xs text-gray-400">Нет фото</span>
@@ -26,22 +26,22 @@ export function MenuItem({item}: MenuItemProps) {
             </div>
 
             <div className="flex flex-col flex-1 pt-2">
-                <h3 className="line-clamp-2 text-xl font-bold leading-tight text-text">
+                <h3 className="line-clamp-2 md:text-xl font-bold leading-tight text-text">
                     {item.name}
                 </h3>
 
-                <p className="line-clamp-2 mt-2 text-text-secondary">
+                <p className="line-clamp-2 mt-2 text-sm md:text-base text-text-secondary">
                     {item.description}
                 </p>
 
                 <div className="mt-auto flex items-center justify-between pt-2">
-                    <span className="text-lg font-black text-text">
+                    <span className="md:text-lg text-sm font-black text-text">
                         {item.price} ₽
                     </span>
 
                     <button
-                        className="surface-soft cursor-pointer flex h-8 w-8 items-center justify-center rounded-full text-text bg-primary hover:bg-secondary transition-colors">
-                        <Plus className="h-5 w-5 sm:h-6 sm:w-6"/>
+                        className="surface-soft cursor-pointer flex h-6 w-6 md:h-8 md:w-8 items-center justify-center rounded-full text-text bg-primary hover:bg-secondary transition-colors">
+                        <Plus className="h-5 w-5 md:h-6 md:w-6"/>
                     </button>
                 </div>
             </div>
--- feautures/screens/main/menu/MenuSection.tsx
+++ feautures/screens/main/menu/MenuSection.tsx
@@ -5,14 +5,14 @@ import {MenuItem} from "@/feautures/screens/main/menu/MenuItem";
 export function MenuSection() {
     return (
         <div
-            className={'mt-8'}
+            className={'md:mt-8'}
         >
             {menus.map((menu) => (
                 <div
                     key={menu.id}
                 >
 
-                    <div className="grid grid-cols-3 gap-4">
+                    <div className="grid grid-cols-2 md:grid-cols-3 md:gap-4">
                         {menu.items.map((item) => (
                             <MenuItem
                                 key={item.id}
