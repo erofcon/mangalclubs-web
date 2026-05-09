21ebb8d44471c262c4d98a7f255d4e77295c18ef
erofcon <temirlan.shereuzhev@gmail.com>
2026-05-09 06:55:32 +0300

    some fixed

--- feautures/floating-buttons/CartButton.tsx
+++ feautures/floating-buttons/CartButton.tsx
@@ -37,18 +37,6 @@ export default function FloatingButton() {
 
     return (
         <>
-            <button
-                onClick={scrollToTop}
-                aria-label="Вернуться наверх"
-                className={`fixed right-5 bottom-26 z-50 flex h-12 w-12 cursor-pointer items-center justify-center rounded-md border border-border bg-background text-text transition-all duration-300 hover:border-primary hover:text-primary md:right-8 md:bottom-34
-                ${
-                    isVisible
-                        ? "pointer-events-auto translate-y-0 opacity-100"
-                        : "pointer-events-none translate-y-4 opacity-0"
-                }`}
-            >
-                <ChevronUp size={24} strokeWidth={2.5}/>
-            </button>
 
             <button
                 data-cart-target="floating"
